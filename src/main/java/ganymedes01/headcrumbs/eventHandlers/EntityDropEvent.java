3c58d5ddc8a60c1df9a0eaa2aff115e6c69f428d
ganymedes01 <foka_12@hotmail.com>
2014-08-22 23:07:54 +0100

    Added compatibility with Tinker's Construct's beheading tools

--- src/main/java/ganymedes01/headcrumbs/eventHandlers/EntityDropEvent.java
+++ src/main/java/ganymedes01/headcrumbs/eventHandlers/EntityDropEvent.java
@@ -1,9 +1,11 @@
 package ganymedes01.headcrumbs.eventHandlers;
 
 import ganymedes01.headcrumbs.Headcrumbs;
+import ganymedes01.headcrumbs.ModItems;
 import ganymedes01.headcrumbs.libs.SkullTypes;
 import ganymedes01.headcrumbs.utils.HeadUtils;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Random;
 
@@ -11,11 +13,14 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EntityDamageSource;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class EntityDropEvent {
@@ -28,13 +33,14 @@ public class EntityDropEvent {
 			return;
 
 		boolean isPoweredCreeper = Headcrumbs.enableChargedCreeperKills && isPoweredCreeper(event.source);
+		int beheading = getBeaheadingLevel(getWeapon(event.source));
 
-		if (isPoweredCreeper || shouldDoRandomDrop(event.entityLiving.worldObj.rand, event.lootingLevel)) {
+		if (isPoweredCreeper || shouldDoRandomDrop(event.entityLiving.worldObj.rand, beheading, event.lootingLevel)) {
 			ItemStack stack = HeadUtils.getHeadfromEntity(event.entityLiving);
 			if (stack == null)
 				return;
 
-			if (isPoweredCreeper || canDropThisHead(stack))
+			if (isPoweredCreeper || canDropThisHead(stack, beheading))
 				if (isPlayerHead(stack) || Headcrumbs.enableMobsAndAnimalHeads)
 					addDrop(stack, event.entityLiving, event.drops);
 		}
@@ -50,16 +56,50 @@ public class EntityDropEvent {
 		return false;
 	}
 
-	private boolean canDropThisHead(ItemStack stack) {
-		return stack.getItem() == Items.skull ? stack.getItemDamage() != 1 && Headcrumbs.enableVanillaHeadsDrop : true;
+	private boolean canDropThisHead(ItemStack head, int beheading) {
+		if (beheading > 0)
+			return head.getItem() != Items.skull && !isPlayerHead(head);
+
+		return head.getItem() == Items.skull ? head.getItemDamage() != 1 && Headcrumbs.enableVanillaHeadsDrop : true;
 	}
 
 	private boolean isPlayerHead(ItemStack stack) {
-		return stack.getItemDamage() == SkullTypes.player.ordinal();
+		return stack.getItem() == ModItems.skull && stack.getItemDamage() == SkullTypes.player.ordinal();
 	}
 
-	private boolean shouldDoRandomDrop(Random rand, int looting) {
+	private boolean shouldDoRandomDrop(Random rand, int beheading, int looting) {
+		if (beheading > 0)
+			return rand.nextInt(100) < beheading * 10;
+
 		return Headcrumbs.enableRandomHeadDrop && rand.nextInt(Headcrumbs.headDropChance / Math.max(looting, 1)) == 0;
+	}
+
+	private int getBeaheadingLevel(ItemStack weapon) {
+		if (Loader.isModLoaded("TConstruct"))
+			try {
+				Class<?> TinkerTools = Class.forName("tconstruct.tools.TinkerTools");
+				Field field = TinkerTools.getDeclaredField("cleaver");
+				field.setAccessible(true);
+				Item cleaver = (Item) field.get(null);
+
+				int beheading = weapon.getTagCompound().getCompoundTag("InfiTool").getInteger("Beheading");
+				if (cleaver == weapon.getItem())
+					beheading += 2;
+
+				return beheading;
+			} catch (Exception e) {
+			}
+
+		return 0;
+	}
+
+	private ItemStack getWeapon(DamageSource source) {
+		if (source instanceof EntityDamageSource) {
+			Entity entity = ((EntityDamageSource) source).getEntity();
+			if (entity instanceof EntityPlayer)
+				return ((EntityPlayer) entity).getCurrentEquippedItem();
+		}
+		return null;
 	}
 
 	private void addDrop(ItemStack stack, EntityLivingBase entity, List<EntityItem> list) {
