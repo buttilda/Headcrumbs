3f53689364f038d2bd73ca7051da08ee1e78045e
ganymedes01 <foka_12@hotmail.com>
2014-12-09 12:49:00 -0200

    Stop using deprecated methods. Added skyboy's head.

--- src/main/java/ganymedes01/headcrumbs/blocks/BlockEmpty.java
+++ src/main/java/ganymedes01/headcrumbs/blocks/BlockEmpty.java
@@ -63,8 +63,8 @@ public class BlockEmpty extends Block {
 	}
 
 	@Override
-	public ItemStack getPickBlock(MovingObjectPosition target, World world, int x, int y, int z) {
-		return world.getBlock(x, y + 1, z).getPickBlock(target, world, x, y + 1, z);
+	public ItemStack getPickBlock(MovingObjectPosition target, World world, int x, int y, int z, EntityPlayer player) {
+		return world.getBlock(x, y + 1, z).getPickBlock(target, world, x, y + 1, z, player);
 	}
 
 	@Override
