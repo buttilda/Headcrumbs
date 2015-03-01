e66aad12ae66c72d48ff9cb603be476d66a995f4
ganymedes01 <foka_12@hotmail.com>
2015-03-01 16:23:50 -0300

    Added Drullkus and Vaht, removed Etho's enchanted axe

--- src/main/java/ganymedes01/headcrumbs/entity/vip/Etho.java
+++ src/main/java/ganymedes01/headcrumbs/entity/vip/Etho.java
@@ -2,7 +2,6 @@ package ganymedes01.headcrumbs.entity.vip;
 
 import ganymedes01.headcrumbs.entity.EntityCelebrity;
 import ganymedes01.headcrumbs.entity.VIPHandler;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -17,7 +16,6 @@ public class Etho extends VIPHandler {
 	@Override
 	public void onSpawn(EntityCelebrity entity) {
 		ItemStack axe = new ItemStack(Items.iron_axe);
-		EnchantmentHelper.addRandomEnchantment(entity.getRNG(), axe, 1 + entity.getRNG().nextInt(30));
 		axe.setStackDisplayName("Treetho's Choppa");
 
 		entity.setCurrentItemOrArmor(0, axe);
