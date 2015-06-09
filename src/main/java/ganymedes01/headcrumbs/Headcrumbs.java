f1c5354221394d8ba031369932e0c44cee5ef27d
ganymedes01 <foka_12@hotmail.com>
2015-06-08 22:30:09 -0300

    Added TheCricket back

--- src/main/java/ganymedes01/headcrumbs/Headcrumbs.java
+++ src/main/java/ganymedes01/headcrumbs/Headcrumbs.java
@@ -100,7 +100,7 @@ public class Headcrumbs {
 	};
 
 	public static String[] modders = {
-		"sinkillerj", "Silentine", "techbrew", "Binnie567", "Adubbz", "Drullkus", "aidancbrady", "Melonar",
+		"sinkillerj", "Silentine", "techbrew", "Binnie567", "Adubbz", "Drullkus", "aidancbrady", "Melonar", "Cricket",
 		"spacetoad", "FyberOptic", "Vexatos", "Mineshopper", "AUTOMATIC_MAIDEN", "sanandreasMC", "FatherToast",
 		"Erasmus_Crowley", "Alblaka", "Emoniph", "Nuchaz", "Kobata", "powercrystals", "AbrarSyed", "sdkillen",
 		"Taelnia", "Lycanite", "asiekierka", "GregoriusT", "skyboy", "Flaxbeard", "Emasher", "joshiejack", "Reika",
