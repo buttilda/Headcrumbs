95adf3d021a7ac357dc7f955abacf8eb7611a6fc
ganymedes01 <foka_12@hotmail.com>
2015-10-25 18:02:27 +0000

    Rorax now drops ender pearls

---
+++ src/main/java/ganymedes01/headcrumbs/entity/vip/Rorax.java
@@ -0,0 +1,14 @@
+package ganymedes01.headcrumbs.entity.vip;
+
+import ganymedes01.headcrumbs.entity.EntityHuman;
+import ganymedes01.headcrumbs.entity.VIPHandler;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
+public class Rorax extends VIPHandler {
+
+	@Override
+	protected ItemStack getItem(EntityHuman entity) {
+		return new ItemStack(Items.ender_pearl);
+	}
+}
