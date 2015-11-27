32e545f6acf1cbbc16d06961a2cfa6873f6edccf
ganymedes01 <foka_12@hotmail.com>
2015-11-27 23:58:21 +0000

    Formatting

--- src/main/java/ganymedes01/headcrumbs/utils/TextureUtils.java
+++ src/main/java/ganymedes01/headcrumbs/utils/TextureUtils.java
@@ -34,18 +34,18 @@ public class TextureUtils {
 			if (profile.getName().equals(minecraft.thePlayer.getGameProfile().getName()))
 				return type == Type.CAPE ? minecraft.thePlayer.getLocationCape() : minecraft.thePlayer.getLocationSkin();
 
-				if (profiles.containsKey(profile.getName())) {
-					Map<?, ?> map = minecraft.func_152342_ad().func_152788_a(profiles.get(profile.getName()));
+			if (profiles.containsKey(profile.getName())) {
+				Map<?, ?> map = minecraft.func_152342_ad().func_152788_a(profiles.get(profile.getName()));
 
-					if (map.containsKey(type))
-						return minecraft.func_152342_ad().func_152792_a((MinecraftProfileTexture) map.get(type), type);
-				} else {
-					// Store profile with self to avoid thread spam
-					profiles.put(profile.getName(), profile);
+				if (map.containsKey(type))
+					return minecraft.func_152342_ad().func_152792_a((MinecraftProfileTexture) map.get(type), type);
+			} else {
+				// Store profile with self to avoid thread spam
+				profiles.put(profile.getName(), profile);
 
-					// Request texture from server
-					PacketHandler.sendToServer(new TextureRequestPacket(profile.getName()));
-				}
+				// Request texture from server
+				PacketHandler.sendToServer(new TextureRequestPacket(profile.getName()));
+			}
 		}
 
 		return type == Type.CAPE ? null : AbstractClientPlayer.locationStevePng;
