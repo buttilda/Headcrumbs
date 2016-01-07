b7593099d9d6c0b350fe75cbc5de3b78c23e513c
ganymedes01 <foka_12@hotmail.com>
2016-01-07 13:36:37 +0000

    Added fry_

--- src/main/java/ganymedes01/headcrumbs/Headcrumbs.java
+++ src/main/java/ganymedes01/headcrumbs/Headcrumbs.java
@@ -116,7 +116,7 @@ public class Headcrumbs {
 		"Erasmus_Crowley", "Alblaka", "Emoniph", "Nuchaz", "Kobata", "powercrystals", "AbrarSyed", "sdkillen",
 		"Taelnia", "Lycanite", "asiekierka", "GregoriusT", "skyboy", "Flaxbeard", "Emasher", "joshiejack", "Reika",
 		"ganymedes01", "Pokefenn", "chylex", "vadis365", "jakimfett", "Lunatrius", "copygirl", "amadornes", "DrZhark",
-		"KeldonSlayer", "KarelMikie3", "Mfernflower", "LuckyLucyF", "Tristaric"
+		"KeldonSlayer", "KarelMikie3", "Mfernflower", "LuckyLucyF", "Tristaric", "fry_"
 	};
 
 	public static String[] youtubers = {
