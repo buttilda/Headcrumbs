6dc37ca68b5c2ac55cbd91e5c13713ddf221647e
Turkey <turkey2349@gmail.com>
2018-12-07 10:24:03 -0500

    Removed debug print statement

--- src/main/java/ganymedes01/headcrumbs/renderers/TileEntityItemStackSkullRenderer.java
+++ src/main/java/ganymedes01/headcrumbs/renderers/TileEntityItemStackSkullRenderer.java
@@ -7,7 +7,6 @@ import net.minecraft.item.ItemStack;
 public class TileEntityItemStackSkullRenderer extends TileEntityItemStackRenderer {
 
 	public void func_192838_a(ItemStack stack, float p_192838_2_) {
-		System.out.println("gere");
 		TileEntityBlockSkullRenderer.INSTANCE.renderHead(0.0F, 0.0F, 0.0F, stack.getMetadata(), 0f,
 				HeadUtils.getModel(stack), HeadUtils.getGameProfile(stack), -1);
 	}
